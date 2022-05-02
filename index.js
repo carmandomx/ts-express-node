e39c9db9f1e3ae72bc661b1cc9080ee2d4c21c9b
Carlos Reyes <carlos.reyes@itksquare.edu.mx>
2022-05-02 08:17:00 -0500

    Preparing deploy to Heroku free tier

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/' , (req, res) => {
     res.send('Config Express + Node + TS')
